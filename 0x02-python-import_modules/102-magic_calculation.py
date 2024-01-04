7611f1d842e49e6d9742953ccd4251dd0b3c6a63
Suvvix <suvvix@gmail.com>
2024-01-04 07:49:28 -0800

    adv

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
-    """Match bytecode."""
-    from magic_calculation_102 import add, sub
-
     if a < b:
+        from magic_calculation_102 import add, sub
         c = add(a, b)
-        for i in range(4, 6):
+        for i in range(4, 7):
             c = add(c, i)
-        return (c)
-
+        return c
     else:
-        return(sub(a, b))
+        return sub(a, b)
