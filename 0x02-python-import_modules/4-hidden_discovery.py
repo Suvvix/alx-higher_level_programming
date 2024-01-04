f89bc0199f43fab847be109473fd3ece529db191
Suvvix <suvvix@gmail.com>
2024-01-04 04:56:14 -0800

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     """Print all hidden directories"""
     import hidden_4
 
-    for i in dir(hidden_4):
-        if 1[:2] != "__":
-            print(i)
+    names = dir(hidden_4)
+    for names in names:
+        if name[:2] != "__":
+            print(name)
