ab7be73eb068aca9346d5ec3ca96f0dd6426ad15
Suvvix <suvvix@gmail.com>
2024-01-10 13:59:32 -0800

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique = set(my_list)
+    uniqlist = set(my_list)
     x = 0
 
-    for i in unique:
+    for i in uniqlist:
         x += i
 
-        return (x)
+    return (x)
