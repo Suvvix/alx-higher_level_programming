cd1f9f5e2d7fd73537ecb80beea2a61c609634c8
Suvvix <suvvix@gmail.com>
2024-01-10 15:21:45 -0800

    7

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    keys = list(a_dictionary.keys())
+    list_keys = list(a_dictionary.keys())
 
-    for value in keys:
-        if value == a_dictionary.get(value):
-            del a_dictionary[value]
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
 
     return (a_dictionary)
