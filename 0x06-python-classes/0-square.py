85e45f1a3894479fbe8c923148f7bc8fb8eb7c7b
Suvvix <suvvix@gmail.com>
2024-01-23 17:22:09 -0800

    0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,9 @@
-To https://github.com/Suvvix/alx-higher_level_programming.git
-   a0ec43f..3498222  master -> master
-root@fc9948a39c47:/alx-higher_level_programming/0x06-python-classes# vi 0-square.py
 #!/usr/bin/python3
 
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
     pass
+
